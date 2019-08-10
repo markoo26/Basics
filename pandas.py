6fb9a13e2d0ed2cff9166f6bc916517adefb402e
markoo26 <markpyt1992@gmail.com>
2019-08-10 08:47:05 +0200

    Second version of the script
    Further code update (till point 60)

--- pandas.py
+++ pandas.py
@@ -4,7 +4,7 @@
 
 ##### Related documentation #####
 import webbrowser
-webbrowser.open_url("https://pandas.pydata.org/pandas-docs/stable/getting_started/10min.html")
+webbrowser.open_new_tab("https://pandas.pydata.org/pandas-docs/stable/getting_started/10min.html")
 
 ##### Import of the libraries #####
 
@@ -64,4 +64,33 @@ df.iat[1,1] #!# faster access
 df[df.B < 0] #!# Select indexes, where column B value is lower than 0
 df[df > 0] #!# Print NaNs where the condition is not met.
 
-=> 42
+##### Filtering (similar to 'IN' clause from SQL) #####
+
+df2 = df.copy()
+df2['E'] = ['one', 'one', 'two', 'three', 'four', 'three']
+df2[df2['E'].isin(['two', 'four'])] #!# searching for the two values only
+
+##### Adding series as new column in the DataFrame #####
+
+s1 = pd.Series([1, 2, 3, 4, 5, 6], index=pd.date_range('20130102', periods=6))
+df['F'] = s1
+
+df.at[dates[0], 'A'] = 1500 #!# Set values by label
+df.iat[0,1] = 2000 #!# By position
+df.loc[:, 'D'] = np.array([5] * len(df)) #!# Assigning numPY array, here: repeat 5 times how many indices are available
+df
+
+df3 = df.copy()
+df3[df3 > -1] = 'Low Value' #!# Equivalent to 'where' operation
+
+##### Reindexing data #####
+
+df1 = df.reindex(index=dates, columns=list(df.columns) + ['E'])
+df1.loc[dates[1]:dates[3], 'E'] = 1
+
+##### Missing values #####
+
+clear_df = df1.dropna(how='any') #!# Drop them
+clear_df2 = df1.fillna(value=5) #!# Fill with one value
+
+pd.isna(df1) #!# Check missing values 
